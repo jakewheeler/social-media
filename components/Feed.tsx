e20d92d5146009385219df6513d9af4f93a0c9d3
Jake Wheeler <jakewheeler17@gmail.com>
2020-05-25 17:49:22 -0400

    fixed avatar

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,4 +1,12 @@
-import { List, ListItem, Box, Stack, Text } from '@chakra-ui/core';
+import {
+  List,
+  ListItem,
+  Box,
+  Stack,
+  Text,
+  Avatar,
+  Flex,
+} from '@chakra-ui/core';
 
 type FeedProps = {
   children?: any;
@@ -8,23 +16,46 @@ export function Feed({ children }: FeedProps) {
   return <List>{children}</List>;
 }
 
-export function FeedItem() {
+type FeedItemProps = {
+  name: string;
+  handle: string;
+  content: string;
+};
+
+export function FeedItem({ name, handle, content }: FeedItemProps) {
   return (
-    <ListItem>
+    <ListItem marginTop={1}>
       <Box
-        h='100px'
-        w='100%'
+        boxSizing='content-box'
         borderWidth='1px'
         borderStyle='solid'
         borderColor='#141d26'
       >
-        <Stack spacing={1} marginLeft='5px'>
-          <Stack isInline>
-            <Text color='white'>User name</Text>
-            <Text color='grey'>@username</Text>
+        <Flex margin={2}>
+          <Avatar
+            name={name}
+            src='https://pbs.twimg.com/profile_images/1173439339898056704/AVWXSQ_f_400x400.jpg'
+          />
+          <Stack spacing={0} marginLeft={5}>
+            <Stack isInline>
+              <Text color='white'>{name}</Text>
+              <Text color='grey'>{handle}</Text>
+            </Stack>
+            <Text color='white' marginTop={3}>
+              {/* {content} */}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Text>
           </Stack>
-          <Text color='white'>Some sort of status</Text>
-        </Stack>
+        </Flex>
       </Box>
     </ListItem>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,21 +6,41 @@ import { Feed, FeedItem } from '../components/Feed';
 export default function Index() {
   return (
     <Layout>
-      <Flex align='center'>
+      <Flex direction='row'>
         <Box w='100%'>
           <Menu>
             <MenuItem text='Home' link='/' />
             <MenuItem text='Explore' link='/explore' />
-            <MenuItem text='Notifications' link='notifications' />
+            <MenuItem text='Notifications' link='/notifications' />
             <MenuItem text='Messages' link='/messages' />
             <MenuItem text='Lists' link='/lists' />
             <MenuItem text='Profile' link='/profile' />
             <MenuItem text='More' link='/more' />
           </Menu>
         </Box>
-        <Box w='100%'>
+        <Box w='100%' overflow='scroll'>
           <Feed>
-            <FeedItem />
+            <FeedItem name={'Jake'} handle={'@jake'} content={'some status'} />
+            <FeedItem
+              name={'Jake2'}
+              handle={'@jake2'}
+              content={'some status'}
+            />
+            <FeedItem
+              name={'Jake2'}
+              handle={'@jake2'}
+              content={'some status'}
+            />
+            <FeedItem
+              name={'Jake2'}
+              handle={'@jake2'}
+              content={'some status'}
+            />
+            <FeedItem
+              name={'Jake2'}
+              handle={'@jake2'}
+              content={'some status'}
+            />
           </Feed>
         </Box>
         <Box w='100%' bg='pink'>
