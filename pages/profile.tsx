e1102a2b431c188fd828bd0e0c2276dd26c7312a
Jake Wheeler <jakewheeler17@gmail.com>
2020-06-09 20:15:59 -0400

    fix profile causing build to fail

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,10 +1,5 @@
 import { Box } from '@chakra-ui/core';
-import { FeedItemProps } from '../components/Feed';
 
-type ProfileProps = {
-  userSeed: string;
-};
-
-export default function Profile({ userSeed }: ProfileProps) {
-  //   return <Box>{appUser.name}</Box>;
+export default function Profile() {
+  return <Box>Example stuff here!</Box>;
 }
